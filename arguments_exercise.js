809c4a0d445d2189cb7165f26e363001c47006ed
Kahaan <kahaan.r.patel@gmail.com>
2018-06-25 13:54:19 -0700

    arguments exercise

--- arguments_exercise.js
+++ arguments_exercise.js
@@ -1,45 +1,61 @@
-function foo(arg) {
-  return arg;
+function sum(nums) {
+  let args = Array.from(arguments)
+  return args.reduce((acc,el) => {return acc + el})
 }
 
-foo(5); // => 5
-foo(); // => undefined
-
-class Cat {
-  constructor(name) {
-    this.name = name;
-  }
-
-  says(sound, person) {
-    console.log(`${this.name} says ${sound} to ${person}!`);
-    return true;
-  }
+function sum(...nums) {
+  let args = nums.reduce((acc,el) => {return acc + el})
+  return args
 }
 
-const markov = new Cat("Markov");
-const breakfast = new Cat("Breakfast");
 
-markov.says("meow", "Ned");
-// Markov says meow to Ned!
-// true
+Function.prototype.myBind1 = function (ctx) {
+  const fn = this;
+  const bindArgs = Array.from(arguments).slice(1);
+  return function _boundFn() {
+    const callArgs = Array.from(arguments);
+    return fn.apply(ctx, bindArgs.concat(callArgs));
+  };
+};
 
-// bind time args are "meow" and "Kush", no call time args
-markov.says.myBind(breakfast, "meow", "Kush")();
-// Breakfast says meow to Kush!
-// true
+Function.prototype.myBind2 = function (ctx, ...bindArgs) {
+  return (...callArgs) => this.apply(ctx, bindArgs.concat(callArgs));
+};
 
-// no bind time args (other than context), call time args are "meow" and "me"
-markov.says.myBind(breakfast)("meow", "a tree");
-// Breakfast says meow to a tree!
-// true
+function curriedSum(numArgs) {
+  const numbers = [];
 
-// bind time arg is "meow", call time arg is "Markov"
-markov.says.myBind(breakfast, "meow")("Markov");
-// Breakfast says meow to Markov!
-// true
+  function _curriedSum(num) {
+    numbers.push(num);
 
-// no bind time args (other than context), call time args are "meow" and "me"
-const notMarkovSays = markov.says.myBind(breakfast);
-notMarkovSays("meow", "me");
-// Breakfast says meow to me!
-// true
+    if (numbers.length === numArgs) {
+      let total = 0;
+
+      numbers.forEach((n) => { total += n; });
+
+      return total;
+    } else {
+      return _curriedSum;
+    }
+  }
+
+  return _curriedSum;
+}
+
+// using spread
+Function.prototype.curry = function (numArgs) {
+  const args = [];
+  const fn = this;
+
+  function _curriedFn(arg) {
+    args.push(arg);
+
+    if (args.length === numArgs) {
+      return fn(...args);
+    } else {
+      return _curriedFn;
+    }
+  }
+
+  return _curriedFn;
+};
